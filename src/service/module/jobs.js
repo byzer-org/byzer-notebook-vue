cffd7784ffae72e55d0db109c1897c9c66f58786
chengwang15 <66863386+chengwang15@users.noreply.github.com>
2022-03-24 16:18:37 +0800

    archived job history (#156)

--- src/service/module/jobs.js
+++ src/service/module/jobs.js
@@ -2,7 +2,7 @@
 import axios from '../handleService'
 
 export default {
-  getJobList: params => axios.get(`/api/jobs?${params}`),
+  getJobList: ({ params, dataTypeSubUrl }) => axios.get(`/api/jobs${dataTypeSubUrl}?${params}`),
   getCodeById: job_id => axios.get(`/api/job/${job_id}/content`),
   deleteJobs: ids => axios.delete(`/api/jobs/${ids}`),
   getJobLogs: ({ job_id, offset = -1 }) => axios.get(`/api/job/${job_id}/log?offset=${offset}`)
