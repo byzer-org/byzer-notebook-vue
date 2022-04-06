2d07c7e03aa5e9228cc7cda561b01663b1192658
songliting <1341839053@qq.com>
2022-04-06 16:59:48 +0800

    #121 optimize collapse code

--- src/page/Workspace/Notebook/CollapseCode/index.vue
+++ src/page/Workspace/Notebook/CollapseCode/index.vue
@@ -1,30 +1,52 @@
 
 <template>
   <div class="collapse-code">
-    <p class="ellipsis-item hide-code">{{value}}</p>
-    <a class="code-btn" href="javascript:;" @click.stop="toggleShowCode">{{$t('notebook.showCode')}}</a>
+    <div class="collapse-code-content">
+      <CodeEditor :lang="type" ref="codeEditor" :content="editorContent" />
+    </div>
+    <div class="collapse-code-btn mt-12">
+      <el-tag type="danger" v-if="status==='ERROR'">
+        <i class="el-ksd-icon-wrong_fill_16 font16 txt-danger"></i>
+        Error
+      </el-tag>
+      <a class="code-btn" href="javascript:;" @click.stop="toggleShowCode">{{$t('notebook.showCode')}}</a>
+    </div>
   </div>
 </template>
 <script>
 import Vue from 'vue'
-import { Component } from 'vue-property-decorator'
+import { Component, Watch } from 'vue-property-decorator'
+import CodeEditor from './Editor.vue'
 
 @Component({
-  props: ['value']
+  props: ['value', 'type', 'status'],
+  components: {
+    CodeEditor
+  }
 })
 export default class CollapseCode extends Vue {
+  editorContent = ''
   toggleShowCode () {
     this.$emit('toggleShowCode')
+  }
+  @Watch('value', { immediate: true })
+  onValueChange (newVal) {
+    this.getEditorContent(newVal)
+  }
+  getEditorContent (value) {
+    if (value) {
+      const valArr = value.split('\n').filter((v, index) => index < 2)
+      this.$nextTick(() => {
+        this.editorContent = valArr.join('\n')
+      })
+    }
   }
 }
 </script>
 <style lang="scss">
 @import '../../../../assets/css/variable.scss';
 .collapse-code {
-  .ellipsis-item {
-    padding-bottom: 15px;
-    width: 200px;
-  }
+  padding-top: 20px;
   .code-btn {
     padding-left: 10px;
     font-size: 12px;
